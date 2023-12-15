23f8557372552073aca06ce766aa3a940cd1ad94
Marek Goliaš <golias.marek.06@gmail.com>
2023-12-15 11:51:44 +0100

    eshop

---
+++ eshop/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from . models import *
+
+def vypis_produktov(request):
+    produkty = Product.objects.all().order_by('nazov')
+    return render(request, 'eshop/index.html', {'produkty':produkty})
+
+def vypis_kategorii(request):
+    category = Category.objects.all().order_by('nazov')
+    return render(request, 'eshop/index.html', {"category":category})
+
+def vypis_eshopu(request):
+    produkty = Product.objects.all().order_by('nazov')
+    category = Category.objects.all().order_by('nazov')
+    return render(request, 'eshop/index.html', {'produkty':produkty, 'category':category})
+
+def vypis_objednavok(request):
+    objednavky = Objednavka.objects.all().order_by('datum')
+    return render(request, 'eshop/index.html', {'objednavky':objednavky})
+
+def vypis_zakaznikov(request):
+    zakaznici = Zakaznik.objects.all().order_by('priezvisko')
+    return render(request, 'eshop/index.html', {'zakaznici':zakaznici})
