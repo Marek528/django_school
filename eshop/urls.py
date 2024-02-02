ddb3912794b0ae9a426c0f7992c3ac10cb47250a
Marek Goliaš <golias.marek.06@gmail.com>
2024-02-02 11:31:22 +0100

    linky na eshope

--- eshop/urls.py
+++ eshop/urls.py
@@ -4,7 +4,9 @@ from .  import views
 
 urlpatterns = [
     path('', views.vypis_eshopu, name='vypis-eshopu'),
+    path('<produkt>/', views.popis_produktu, name='popis-produktu'),
     path('kategorie/', views.vypis_kategorii, name='vypis-kategorii'),
+    path('kategorie/<kategoria>/', views.vypis_kategoria, name='vypis-kategoria'),
     path('objednavky/', views.vypis_objednavok, name='vypis-objednavok'),
     path('zakaznici/', views.vypis_zakaznikov, name='vypis-zakaznici'),
 ]
--- eshop/views.py
+++ eshop/views.py
@@ -7,7 +7,8 @@ def vypis_kategorii(request):
 
 def vypis_eshopu(request):
     produkty = Product.objects.all().order_by('nazov')
-    return render(request, 'eshop/index.html', {'produkty':produkty})
+    category = Category.objects.all().order_by('nazov')
+    return render(request, 'eshop/index.html', {'produkty':produkty, 'category':category})
 
 def vypis_objednavok(request):
     objednavky = Objednavka.objects.all().order_by('datum')
@@ -16,3 +17,15 @@ def vypis_objednavok(request):
 def vypis_zakaznikov(request):
     zakaznici = Zakaznik.objects.all().order_by('priezvisko')
     return render(request, 'eshop/index.html', {'zakaznici':zakaznici})
+
+def vypis_kategoria(request, kategoria):
+    id_kategoria = Category.objects.get(nazov=kategoria).pk
+    produkty = Product.objects.filter(kategoria_id=id_kategoria).order_by('nazov')
+    produkty_list = []
+    for produkt in produkty:
+        produkty_list.append(produkt)
+    return render(request, 'eshop/produkty_list.html', {"kategoria":kategoria, "produkty":produkty})
+
+def popis_produktu(request, produkt):
+    produkty = Product.objects.filter(id=produkt).order_by('nazov')
+    return render(request, 'eshop/popis_produktu.html', {'produkty':produkty})
